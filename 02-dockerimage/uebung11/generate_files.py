234fabd01735241b887375fe1eb02ca83ea4d2d9
Tomek Wenzel <tomek.wenzel@swisstph.ch>
2026-05-04 14:00:04 +0200

    task: images_02, 9,10

---
+++ 02-dockerimage/uebung11/generate_files.py
@@ -0,0 +1,6 @@
+with open("file1.txt", "w") as f:
+    f.write("This is file 1\n")
+with open("file2.txt", "w") as f:
+    f.write("This is file 2\n")
+with open("file3.txt", "w") as f:
+    f.write("This is file 3\n")
